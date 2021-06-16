e4adda115e084ad3a783353342207724713d39dc
kaustav927 <kaustav927@gmail.com>
2021-06-16 15:26:07 -0400

    backend payment processor

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,66 @@ const{Payment}=require('./database.js')
 const app = new Koa();
 const router = new Router();
 
+const items={
+    '1':{id:1, url:'http://UrlToDownloadItem1'},
+    '2':{id:2, url:'http://UrlToDownloadItem2'}
+};
 router.get('/api/getPaymentId/:itemId',async ctx => {
-    ctx.body='hello world';
-})
+    const paymentId=(Math.random()*10000).toFixed(0);
+    await Payment.create({
+        id: paymentId,
+        itemId: ctx.params.itemId,
+        paid:false
+    });
+    ctx.body={
+        paymentId
+    };
+});
+
+router.get('/api/getItemUrl/:paymentId', async ctx => {
+    const payment= await Payment.findOne({id: ctx.params.paymentId});
+    if(payment && payment.paid === true){
+        ctx.body={
+            url: item[payment.itemId].url
+        };
+    }
+    else{
+        ctx.body={
+            url:''
+        };
+    }
+});
 
 app.use(cors()).use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('Server running on port 4000');
 })
+
+
+const listenToEvents=()=>{
+    const provider = new ethers.providers.JsonRpcProvider('http://localhost:9545');
+    const networkId = '5777';
+
+    const payments = new ethers.Contract(
+        Payments.networks[networkId].address,
+        Payments.abi,
+        provider
+    );
+
+    payments.on('PaymentDone', async (payer, amount, paymentId, date)=>{
+        console.log(`
+            from ${payer}
+            amount ${amount}
+            paymentId ${paymentId}
+            date ${(new Date(date.toNumber()*1000)).toLocalString()}
+        `);
+
+    const payment = await Payment.findOne({id: paymentId});
+    if(payment){
+        payment.paid=true;
+        await payment.save();
+    }
+    });
+};
+listenToEvents();
